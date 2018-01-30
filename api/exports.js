5f3356dba55c570cbe4e604c16f5bbbf0e591c85
Josh Duck <github@joshduck.com>
2018-01-30 13:59:11 +1000

    Update exports.js

--- api/exports.js
+++ api/exports.js
@@ -13,3 +13,45 @@ import {
   getLoadersForComponent, // Get requests for React wrapper
   createComponent
 } from "fetchy-react";
+
+// Internal classes
+
+// Server query context
+
+class Runtime {
+  constructor({ store, resolver })
+  run(request) {} // Query
+}
+
+class Query {
+  fetch() {} // Promise<ResultDescriptor>
+  defer() {} // ResultDescriptor
+  waitForDeferred() {} // Promise<*>
+  waitForRequired() {} // Promise<*>
+}
+
+class Store {
+  insertRecords(data) {}
+  
+  setRecord(descriptor)
+  getRecord(descriptor)
+  hasRecord(descriptor) {} 
+  
+  getValue(descriptor) {}
+  getError(descriptor) {}
+  getLoading(descriptor) {}
+}
+
+class FlushableStore extends Store {
+  flushRecords() {} // Get records that have changed since last flush
+}
+
+class LocalResolver {
+  constructor(loaders)
+  resolve(descriptor)
+}
+
+class NetworkResolver {
+  constructor(config)
+  resolve(descriptor)
+}
